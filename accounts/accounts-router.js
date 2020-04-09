62d1c54022ad58be70f990639ebefacfe7d5e8a4
Robert Kulp <pedalinon@gmail.com>
2020-04-09 11:28:50 -0700

    router file added

---
+++ accounts/accounts-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+// database access using knex
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.status(200).json( 'You found the accounts router!' );
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const accRouter = require("../accounts/accounts-router.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accRouter);
+
+server.get("/", (req, res) => {
+	res.status(200).json( 'you got something here!' );
+});
 
 module.exports = server;
