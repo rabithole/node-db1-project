ac74455bebb4a52ff81e476654e1a465726d8290
Robert Kulp <pedalinon@gmail.com>
2020-04-10 16:01:12 -0700

    mvp

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -30,15 +30,41 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-
+	console.log(req.body)
+	db('accounts').insert(req.body)
+	.then(acc => {
+		res.status(210).json(acc);
+	})
+	.catch(err => {
+		res.status(501).json({ message: 'What the hell do you think your doing?' });
+	})
 });
 
 router.put('/:id', (req, res) => {
+	const { id } = req.params;
+	const changes = req.body;
 
+	db('accounts').where({ id }).update(changes)
+		
+		.then(acc => {
+			res.status(201).json(acc);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'you failed again'});
+		})
 });
 
 router.delete('/:id', (req, res) => {
-	db('accounts').where({ name: 'account-01' });
+	const { id } = req.params;
+
+	db('accounts').delete()
+	.where({ id })
+	.then(acc => {
+		res.status(210).json(acc);
+	})
+	.catch(err => {
+		res.status(500).json({ message: "You've lost your minde!" })
+	})
 });
 
 module.exports = router;
